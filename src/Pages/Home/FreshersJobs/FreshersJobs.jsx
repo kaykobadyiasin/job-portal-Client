aaabc7a7970c53b33c0b3a928ffb23043f5ebf39
EYASIN KHAN <kaykobadyiasin@gmail.com.com>
2023-07-23 00:44:01 +0600

    position job in fresher jobs

---
+++ src/Pages/Home/FreshersJobs/FreshersJobs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import c_img1 from '../../../assets/job-company/2.png'
+import { FaBuilding, FaLocationDot} from 'react-icons/fa6';
+
+const FreshersJobs = ({ freshersJob }) => {
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className='w-6/12 h-[250px] mx-auto'><img className='w-full' src={c_img1} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{freshersJob.positionName}</h2>
+                    <div className='flex items-center'><FaLocationDot></FaLocationDot> &#160; Dhaka, Bangladesh </div>
+                    <div className='flex items-center'><FaBuilding></FaBuilding>&#160; Apple</div>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Apply</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FreshersJobs;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
+import FreshersJobs from '../FreshersJobs/FreshersJobs';
+
 const Home = () => {
+
+
+    const [freshersJobs, setFreshersJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('fresherJobs.json')
+        .then(res => res.json())
+        .then(data => setFreshersJobs(data))
+    }, [])
+
     return (
         <div className='w-10/12 mx-auto my-10'>
             <Tabs>
-                <TabList className='border-b border-[#60A5FA]  pt-5 text-center'>
+                <TabList className='border-b border-[#60A5FA]  pt-5'>
                     <Tab >Job Seeker</Tab>
                     <Tab >Job poster</Tab>
                 </TabList>
 
-                <TabPanel  className=' mt-5'>
-                    <h2 className='border-b w-1/12 text-xl font-semibold'>Fresher Jobs</h2>
+                <TabPanel className='mt-10'>
+                    <h2 className='mx-auto border-b-4 lg:w-[180px] w-[180px] text-3xl text-center border-[#60A5FA] font-semibold'>Fresher Jobs</h2>
+                    <div className='mt-5'>
+                        <div className='hero-content mx-auto grid lg:grid-cols-3 gap-8 justify-center'>
+
+                            {
+                                freshersJobs.map(freshersJob => <FreshersJobs
+                                key={freshersJob.id}
+                                freshersJob={freshersJob}
+                                ></FreshersJobs>)
+                            }
+                            
+                        </div>
+                        <div className='flex justify-center my-5'>
+                            <button className="btn btn-primary">See All Jobs</button>
+                        </div>
+                    </div>
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content 2</h2>
